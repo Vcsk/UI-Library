3b1b49089a47ee772a11b300dc92bef9511218fe
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-01-29 21:33:58 +0800

    Update MyUILib(Unamed).lua

--- Example/MyUILib(Unamed).lua
+++ Example/MyUILib(Unamed).lua
@@ -2,7 +2,7 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/
 local Window = Library:Create("Example") -- any name
 
 local Tab1 = Window:Tab("Tab 1","rbxassetid://10888331510")
-local Tab2 = Window:Tab("Tab 2","rbxassetid://10888331510")
+local Tab2 = Window:Tab("Players","rbxassetid://12296135476")
 
 Tab1:Button("Button 1", function() -- button
 	print("Pressed Button 1!")
@@ -16,18 +16,14 @@ Tab1:Slider("Slider 1", 1,100, function(value) -- slider | min,max
 	print(value)
 end)
 
-Tab2:Button("Button 2", function()
-	print("Pressed Button 2!")
+Tab1:Button("Destroy UI", function()
+	Library:DestroyUI()
 end)
 
-Tab2:Toggle("Toggle 2", function(state)
-	print(state)
-end)
-
-Tab2:Slider("Slider 2", 16,500, function(value)
+Tab2:Slider("WalkSpeed", 16,500, function(value)
 	game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = value
 end)
 
-Tab2:Button("Destroy UI", function()
-	Library:DestroyUI()
+Tab2:Slider("JumpPower", 50,1000, function(value)
+	game:GetService("Players").LocalPlayer.Character.Humanoid.JumpPower = value
 end)
